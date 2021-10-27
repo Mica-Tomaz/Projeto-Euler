0a09681dd46d597d25131328e09537a6c88da327
Mica-Tomaz <93274967+Mica-Tomaz@users.noreply.github.com>
2021-10-27 14:50:36 -0300

    Add files via upload

---
+++ multiplos3_5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    long soma = 0;
+    
+    for (int i = 1; i < 1000; i ++){
+        if(i%3 == 0 || i%5 == 0){
+            soma += i;
+        }
+    }
+
+    cout << soma << endl;
+    return 0;
+}

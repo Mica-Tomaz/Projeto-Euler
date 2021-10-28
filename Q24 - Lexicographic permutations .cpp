e9bb4859afb5d8a99bd4a2b3791fe7924d50f53d
Mica-Tomaz <93274967+Mica-Tomaz@users.noreply.github.com>
2021-10-28 11:10:55 -0300

    Update Q24 - Lexicographic permutations .cpp

--- Q24 - Lexicographic permutations .cpp
+++ Q24 - Lexicographic permutations .cpp
@@ -5,16 +5,16 @@
 #include <algorithm>
 
 
-//questão 24 - Lexicographic permutations
+                                                                    // Questão 24 - Lexicographic permutations
 
 using namespace std;
 using std::find;
 
 int main(void){
-    char x[11] = "0123456789";
-    for(int i = 1; i < 1000000; i++){
-        std::next_permutation(&x[0],&x[10]);
+    char x[11] = "0123456789";                                      // fiz um vetor tipo char com os numeros de 0 até 9
+    for(int i = 1; i < 1000000; i++){                               // faz 1000000 de permutações consecutivas
+        std::next_permutation(&x[0],&x[10]);                        // essa função permuta um vetor ou um trecho de um vetor
     }
-    cout << x << endl;
+    cout << x << endl;                                              // retorna a ultima permutação, que no caso foi a 1000000º permutação
     return 0;
 }

f6d6a79211c2153be3bd37bd622ff4481e6db609
Mica-Tomaz <93274967+Mica-Tomaz@users.noreply.github.com>
2021-10-28 00:23:19 -0300

    Update Q2 - Fibonacci.cpp

--- Q2 - Fibonacci.cpp
+++ Q2 - Fibonacci.cpp
@@ -2,18 +2,18 @@
 #include <vector>
 
 using namespace std;
-
+                                                //Questão 2 - Fibonacci
 int main(void){
     
     vector <long int> fibo;
-    fibo.push_back(1);//adcionando os dois primeiros termos da seuqência de fibonacci citada na questão 
+    fibo.push_back(1);                          //adcionando os dois primeiros termos da seuqência de fibonacci citada na questão 
     fibo.push_back(2);
     long int soma = 2;
     int a = 2;
-    while (fibo[a-1] <= 4000000){//somando os valores pares da sequência de fibonacci
+    while (fibo[a-1] <= 4000000){               //somando os valores pares da sequência de fibonacci
         fibo.push_back(fibo[a-1] + fibo[a-2]);
         a = a + 1;
-        if(fibo[a - 1]%2 == 0){//testando se o numero é par
+        if(fibo[a - 1]%2 == 0){                 //testando se o numero é par
             soma += fibo[a - 1];
         }
     }

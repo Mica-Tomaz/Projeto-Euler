40d2b7fe1052d2825db5b83056667a67db81906b
Mica-Tomaz <93274967+Mica-Tomaz@users.noreply.github.com>
2021-10-28 00:11:48 -0300

    Update and rename Fibonacci.cpp to Q2 - Fibonacci.cpp

--- Q2 - Fibonacci.cpp
+++ Q2 - Fibonacci.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int main(void){
     
     vector <long int> fibo;
-    fibo.push_back(1);
+    fibo.push_back(1);//adcionando os dois primeiros termos da seuqência de fibonacci citada na questão 
     fibo.push_back(2);
     long int soma = 2;
     int a = 2;
-    while (fibo[a-1] <= 4000000){
+    while (fibo[a-1] <= 4000000){//somando os valores pares da sequência de fibonacci
         fibo.push_back(fibo[a-1] + fibo[a-2]);
         a = a + 1;
-        if(fibo[a - 1]%2 == 0){
+        if(fibo[a - 1]%2 == 0){//testando se o numero é par
             soma += fibo[a - 1];
         }
     }

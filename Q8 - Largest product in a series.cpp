5a5316d951ebcda8a3b4d7a6685498cd8a651a17
Mica-Tomaz <93274967+Mica-Tomaz@users.noreply.github.com>
2021-10-28 10:42:18 -0300

    Update and rename Largest product in a series.cpp to Q8 - Largest product in a series.cpp

--- Q8 - Largest product in a series.cpp
+++ Q8 - Largest product in a series.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
 
-//questão 8 - Largest product in a series
+                                                                                        //questão 8 - Largest product in a series
 
 using namespace std;
 
 int main(void){
-    long long int aux1 = 1, aux2 = 0;
+    long long int aux1 = 1, aux2 = 0;                                                   // crio duas variáveis auxiliares para armazenamento
     int j = 0;
-    char vetor [] = "73167176531330624919225119674426574742355349194934"
+    char vetor [] = "73167176531330624919225119674426574742355349194934"                // como o numero é muito grande, adcionei ele como um array do tipo char
                     "96983520312774506326239578318016984801869478851843"
                     "85861560789112949495459501737958331952853208805511"
                     "12540698747158523863050715693290963295227443043557"
@@ -28,15 +28,15 @@ int main(void){
                     "84580156166097919133875499200524063689912560717606"
                     "05886116467109405077541002256983155200055935729725"
                     "71636269561882670428252483600823257530420752963450";
-    while(j <= 987){
+    while(j <= 987){                                                                    // como a questão pede uma sequência de 13 numeros consecutivos, o primeiro numero dessa sequencia deve ser no máximo o 987º numero 
         for(int i = 0 + j ; i < 13 + j; i++){
-            aux1 = aux1*(vetor[i] - '0');
+            aux1 = aux1*(vetor[i] - '0');                                               // esse for faz o produto da sequencia de 13 numeros consecutivos e a salva em aux1
         }
-        if(aux1 > aux2){
+        if(aux1 > aux2){                                                                // salva o maior dos valor dos produtos em aux2
             aux2 = aux1;
         }
         aux1 = 1;
-        j ++;
+        j ++;                                                                           // adciona 1 ao j para pegar a próxima sequência de 13 numeros
     }
     cout << aux2 << endl;
     return 0;
